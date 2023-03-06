64bb2ea18a012877baae17bf41fe3fcad7cee8e1
SainiRajat1407 <105829997+SainiRajat1407@users.noreply.github.com>
2023-03-06 15:44:10 +0000

    working on calcuating the percentage
    Co-authored-by: Olaseni Bello <Bello-online@users.noreply.github.com>

--- Src/script/script.js
+++ Src/script/script.js
@@ -1,39 +1,40 @@
-const firstName = document.getElementById("nameOne");
-const secondName = document.getElementById('nameTwo');
-const submitBtn = document.getElementById('submitButton');
-const resultName1 = document.getElementById('resultName1');
-const resultName2 = document.getElementById('resultName2');
-discardedCharacters = []
-numArray = []
 
+
+function tfName1(){
+  const firstName = document.getElementById("nameOne");
+  const resultName1 = document.getElementById('resultName1');
+  localStorage.setItem('name1',firstName.value);
+  if (firstName.value == '') {
+    resultName1.innerHTML = "First name is empty!"
+  }
+  
+}
+function tfName2(){
+  const secondName = document.getElementById('nameTwo');
+  const resultName2 = document.getElementById('resultName2');
+  localStorage.setItem('name2',secondName.value)
+  
+  if (secondName.value == "") {
+    resultName2.innerHTML = "Second name is empty"
+  }
+  
+}
 
 //Form validation
 
 function formValidator() {
-  let name1 = firstName.value;
-  let name2 = secondName.value;
-  if (name1 == '') {
-    resultName1.innerHTML = "First name is empty!"
-  }
-  if (name2 == "") {
-    resultName2.innerHTML = "Second name is empty"
-  }
+  tfName1();
+  tfName2();
+  
 }
 
-//method to check the canceled out characters while doing the love calculations
-function charactersCheck(check) {
-  result = false
-  for (i = 0; i < discardedCharacters.length; i++) {
-    if (check == discardedCharacters[i]) {
-      result = true;
-    }
-  } return result;
-}
+
 
 //function that creates the numArray by comparing the names
 function comparingNames(name1, name2) {
   loveVar = "loves"
-  
+  const numArray = []
+  const discardedCharacters = []
 
   const combinedName = name1 + loveVar + name2;
   console.log(combinedName)
@@ -41,16 +42,22 @@ function comparingNames(name1, name2) {
   for (i = 0; i < combinedName.length; i++) {
     let counter = 1;
     let counter2 = 0
-    if(i == combinedName.length-1 && !charactersCheck(combinedName[combinedName.length-1])){
+
+    testCharacters = false
+    for (k = 0; k < discardedCharacters.length; k++) {
+      if (combinedName[i] == discardedCharacters[k]) {
+        testCharacters = true;
+      }
+    }
+
+    if(i == combinedName.length-1 && !testCharacters){
       numArray.push(1);
     }
 
-    if (!charactersCheck(combinedName[i])) {
+    else if (!testCharacters) {
       for (j = i + 1; j < combinedName.length; j++) {
-        console.log(combinedName[i] == combinedName[j])
         if (combinedName[i] == combinedName[j]) {
           counter += 1;
-          console.log(counter)
         }
         else {
           counter2 = 1
@@ -59,19 +66,21 @@ function comparingNames(name1, name2) {
       
       if (counter > 1) {
         numArray.push(counter)
-        console.log("counter")
       }
       else if (counter2 == 1) {
         numArray.push(counter2)
-        console.log("counter2")
       }
     }
-
-
     discardedCharacters.push(combinedName[i]);
 
-  } console.log(discardedCharacters)
+  } console.log(discardedCharacters);
   console.log(numArray)
+  for(i=discardedCharacters.length;i>0;i--){
+    discardedCharacters.pop();
+  }
+  console.log(discardedCharacters);
+
+  return numArray;
 }
 
 //@desc function that caluclates the love percentage using the numArray(while comparing) 
@@ -81,36 +90,37 @@ function calculatePercentage(numArray) {
    numArrayLength = numArray.length;
   //this to decide thow many times a loop will run
    numSteps = Math.round((numArrayLength / 2)) - 1;
-   console.log(numSteps)
 
-   sumArray = []
+   stepsArray = []
+    stepsArray.push(numArray);
+    console.log(stepsArray)
+
    for (k = 0; k < numSteps; k++) {
-     tempArray = []
-     for (i = 0; i < numArray.length; i++) {
+     let tempArray = []
+     for (i = 0; i < stepsArray[k].length; i++) {
 
-       if (numArray.length - 1 - i > i) {
-         let sum = numArray[i] + numArray[numArray.length - i - 1];
+       if (stepsArray[k].length - 1 - i > i) {
+         let sum = stepsArray[k][i] + stepsArray[k][numArray.length - i - 1];
          tempArray.push(sum);
        }
-       else if (numArray.length - i - 1 == i) {
-         let noSum = numArray[i];
+       else if (stepsArray[k].length - i - 1 == i) {
+         let noSum = stepsArray[k][i];
          tempArray.push(noSum);
        }
 
      }
-     sumArray.push(tempArray);
-     console.log(sumArray)
+     console.log(tempArray)
+     stepsArray.push(tempArray);
    }
-
+   console.log(stepsArray);
 }
 
 //@desc function that compares the names and calculates the percentatge
 function loveCalulation() {
-  name1 = firstName.value.toLowerCase();
-  name2 = secondName.value.toLowerCase();
-  comparingNames(name1, name2);
-
-  // calculatePercentage(numArray);
+  name1 = localStorage.getItem('name1').toLowerCase();
+  name2 = localStorage.getItem('name2').toLowerCase();
+  let numArray = comparingNames(name1, name2);
+  calculatePercentage(numArray);
 }
 
 //main function that run when the submit button is pressed
